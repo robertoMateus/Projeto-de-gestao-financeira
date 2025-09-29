d2d30fddc191d54bc8f6dcfb5cee467b7ca5e3e9
robertoMateus <robertomateusdemelosilva@gmail.com>
2025-09-28 21:54:29 -0300

    Criação de componentes de alerta, de api, e de loading para frontEnd

---
+++ backEnd/controle-financeiro/src/main/java/com/gestao/controleFinanceiro/Config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.gestao.controleFinanceiro.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+     WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
